731d5db2450ea90988de4cc7b95781347cec7d5e
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-05-25 23:53:43 -0500

    Update CMakeLists, add more info on Misc

--- include/Misc/Primitives.hpp
+++ include/Misc/Primitives.hpp
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 
+/********************************************************************
+* Note: These are just my default primitives to just drop and use
+* Mostly inspired by the T&L stuff since is just all I need
+********************************************************************/
+
 // Just a single point
 struct TVertex2D {
 	float x = 0.0f;
@@ -11,10 +16,15 @@ struct TVertex2D {
 
 // Single point, with texture coordinates and color
 struct TLVertex2D {
+	// Vertex position
 	float x = 0.0f;
 	float y = 0.0f;
+
+	// Texture coordinates
 	float u = 0.0f;
 	float v = 0.0f;
+
+	// Color
 	uint32_t color = 0xffffffff;
 };
 
@@ -27,12 +37,22 @@ struct TVertex3D {
 
 // 3D point with texture coordinates and color
 struct TLVertex2D {
+	// Vertex position
 	float x = 0.0f;
 	float y = 0.0f;
 	float z = 0.0f;
+	
+	// Texture coordinates
 	float u = 0.0f;
 	float v = 0.0f;
+
+	// Diffuse color
 	uint32_t color = 0xffffffff;
+
+	// Normals
+	float i = 0.0f;
+	float j = 0.0f;
+	float k = 0.0f;
 };
 
 #endif
