4fb055b0e03b74e0721e66d5b81830ee2c7584e4
Clb184 <84605945+Clb184@users.noreply.github.com>
2025-07-20 20:04:49 -0500

    Add optional width and height parameters when loading textures

--- include/OpenGL/Texture.hpp
+++ include/OpenGL/Texture.hpp
@@ -4,7 +4,7 @@
 #include <gl/glew.h>
 
 namespace Clb184 {
-	bool LoadTextureFromFile(const char* name, GLuint* tex_unit);
+	bool LoadTextureFromFile(const char* name, GLuint* tex_unit, int* width, int* height); // Simply loads a texture from file, optionally get the size of it via pointers
 }
 
 #endif
