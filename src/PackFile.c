5f4d489d312abd2ade9bcc028ad1f880780588ef
Clb184 <84605945+Clb184@users.noreply.github.com>
2026-04-16 20:05:56 -0500

    Begin entry deflate implementation

--- src/PackFile.c
+++ src/PackFile.c
@@ -28,6 +28,54 @@ int PackFileAddEntryFromFile(pack_file_t* pack_file, const char* filename){
 	char buf[1024];
 	sprintf(buf, "Adding entry with data from file \"%s\" to packed file", filename);
 	LOG_INFO(buf);
+
+	// Relevant structs
+	z_stream stream;
+	FILE* src = 0;
+	uint8_t* data = 0;
+	uint8_t* data_out = 0;
+	size_t size = 0;
+
+	src = fopen(filename, "rb");
+	if(0 == src) return -1; // Failed to open file
+	
+	fseek(src, 0, SEEK_END);
+	size = ftell(src);
+	rewind(src);
+	sprintf(buf, "Entry file size: %lld", size);
+	LOG_INFO(buf);
+	
+	data = calloc(size, 1);
+	data_out = calloc(128 * 1024, 1);
+
+	if(0 == data || 0 == data_out) return -2; // Failed to allocate memory
+	
+
+	// Begin deflate
+	if(Z_OK == deflateInit(&stream, -1)) {
+		LOG_INFO("Begin entry compression");
+		stream.data_type = Z_BINARY;
+		while(stream.total_in != size) {
+			stream.avail_in = fread(data, size, 1, src);
+			stream.next_in = data;
+			while(0 < stream.avail_in) {
+				stream.avail_out = 128 * 1024;
+				stream.next_out = data_out;
+				deflate(&stream, Z_NO_FLUSH);
+				sprintf(buf, "Available out bytes after deflate: %lld", stream.avail_out);
+				LOG_INFO(buf);
+
+				sprintf(buf, "Available in bytes after deflate: %lld", stream.avail_in);
+				LOG_INFO(buf);
+			}
+		}
+		if(Z_OK == deflateEnd(&stream)) {
+			sprintf(buf, "Entry added successfuly, compressed to %lld bytes", stream.total_out);
+			LOG_INFO(buf);
+		}
+	} else {
+		LOG_ERROR("Error on initializing deflate operation");
+	}
 	return 0;
 }
 
