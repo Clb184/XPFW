e278fca5f307c2b808d6c0ad5f1c3a1c14da48bb
Clb184 <84605945+Clb184@users.noreply.github.com>
2026-04-23 12:02:35 -0500

    Implement loading Vorbis file from memory

--- src/OGGDecode.c
+++ src/OGGDecode.c
@@ -9,35 +9,67 @@ typedef struct {
 	char* source_data;
 	char* move_data;
 	size_t offset;	
+	size_t size;
 } vorbis_mem_t;
 
 size_t ReadVorbisData(void* data, size_t size, size_t nmemb, void* source) {
-	printf("data: %p size: %lld nmemb: %lld source: %p\n", data, size, nmemb, source);
-	vorbis_mem_t* vm = (vorbis_mem_t*source)
+	//printf("ReadVorbisData()\n");
+	//printf("data: %p size: %lld nmemb: %lld source: %p\n", data, size, nmemb, source);
 #ifdef _MSC_VER
 #pragma message("Adding debug break (MSVC)")
-	__debugbreak();
+	//__debugbreak();
 #endif
-
-	return 0;
+	if(0 == source) return -1;
+	vorbis_mem_t* vm = (vorbis_mem_t*)source;
+	//printf("offset: %lld size: %lld\n", vm->offset, vm->size);
+	size_t to_read = (nmemb * size) > (vm->size - vm->offset) ? vm->size - vm->offset : nmemb * size;
+	//printf("to_read returned: %lld\n", to_read);
+	memcpy(data, vm->move_data, to_read);
+	vm->move_data += to_read;
+	vm->offset += to_read;
+	return to_read;
 }
 
 int SeekVorbisData(void* source, ogg_int64_t offset, int whence) {
-	printf("source: %p offset: %I64d whence: %d\n", source, offset, whence);
+	//printf("SeekVorbisData()\n");
+	//printf("source: %p offset: %I64d whence: %d\n", source, offset, whence);
 #ifdef _MSC_VER
 #pragma message("Adding debug break (MSVC)")
-	__debugbreak();
+	//__debugbreak();
 #endif
+	if(0 == source) return -1;
+	vorbis_mem_t* vm = (vorbis_mem_t*)source;
+	switch(whence) {
+		case 0: // SET
+			vm->move_data = vm->source_data + offset;
+			vm->offset = offset;
+			break;
+		case 1: // CUR
+			vm->move_data += offset;
+			vm->offset += offset;
+			break;
+		case 2: // END
+			vm->move_data = vm->source_data + vm->size + offset;
+			vm->offset = vm->size + offset;
+			break;
+		default:
+		       printf("Whence was: %d\n", whence);	
+			return -1;
+	}
 	return 0;
 }
 
 long TellVorbisPos(void* source) {
-	printf("source:%p\n", source);
+	//printf("TellVorbisPos()\n");
+	//printf("source: %p\n", source);
 #ifdef _MSC_VER
 #pragma message("Adding debug break (MSVC)")
-	__debugbreak();
+	//__debugbreak();
 #endif
-	return 0;
+	if(0 == source) return -1;
+	vorbis_mem_t* vm = (vorbis_mem_t*)source;
+	//printf("Offset: %lld\n", vm->offset);
+	return vm->offset;
 }
 
 bool LoadVorbisFile(const char* filename, vorbis_data_t* vorbis_data) {
@@ -59,19 +91,6 @@ bool LoadVorbisFile(const char* filename, vorbis_data_t* vorbis_data) {
 		free(vf);
 		return false;
        }
-
-       /*if (!(fp = fopen(filename, "rb"))) {
-		sprintf(buf, "Vorbis file \"%s\" does not exist", filename);
-		LOG_ERROR(buf);
-		return false;
-       }*/
-
-       LOG_INFO("Testing file");
-       /*if ((ov_open_callbacks(fp, vf, NULL, 0, OV_CALLBACKS_NOCLOSE) < 0)) {
-           printf("Invalid OGG file.\n");
-           free(vf);
-           return false;
-       }*/
 
        vi = ov_info(vf, -1);
        int64_t sample_count = ov_pcm_total(vf, -1);
@@ -109,12 +128,19 @@ bool LoadVorbisFile(const char* filename, vorbis_data_t* vorbis_data) {
 bool LoadVorbisFromMemory(char* data, size_t size, vorbis_data_t* vorbis_data) {
 	char buf[256] = "";
 	int result = 0;
-	LOG_INFO("Loading Vorbis data from memory");
+	sprintf(buf, "Loading Vorbis data from memory (%lld bytes)", size);
+	LOG_INFO(buf);
 	assert(0 != vorbis_data);
 	ov_callbacks data_read = { 0 };
+	vorbis_mem_t mem = { 0 };
 	data_read.read_func = ReadVorbisData;
 	data_read.seek_func = SeekVorbisData;
 	data_read.tell_func = TellVorbisPos;
+	
+	mem.source_data = data;
+	mem.move_data = data;
+	mem.offset = 0;
+	mem.size = size;
 
        // Decode all the data with this simple function
        OggVorbis_File* vf = 0;
@@ -123,8 +149,8 @@ bool LoadVorbisFromMemory(char* data, size_t size, vorbis_data_t* vorbis_data) {
        if (0 == (vf = calloc(1, sizeof(OggVorbis_File)))) return false;
 
        LOG_INFO("Testing file");
-       printf("data: %p vf: %p\n", data, vf);
-       result = ov_open_callbacks(data, vf, data, size, data_read);
+       //printf("data: %p vf: %p mem: %p\n", data, vf, &mem);
+       result = ov_open_callbacks(&mem, vf, data, size, data_read);
        if (result < 0) {
            sprintf(buf, "Invalid OGG data ID: %d", result);
            free(vf);
@@ -140,6 +166,7 @@ bool LoadVorbisFromMemory(char* data, size_t size, vorbis_data_t* vorbis_data) {
 
 	LOG_INFO("Allocating sample data");       
         int16_t* samples = calloc(sizeof(int16_t), sample_count * channels);
+	//printf("samples: %p\n", samples);
 
        if (0 == samples) {
            ov_clear(vf);
@@ -151,7 +178,7 @@ bool LoadVorbisFromMemory(char* data, size_t size, vorbis_data_t* vorbis_data) {
        int bitstr = 0;
        long ret = 0;
        char* seek = (char*)samples;
-	printf("seek: %p\n", seek);
+	//printf("seek: %p\n", seek);
        do {
            ret = ov_read(vf, seek, 4096, 0, channels, 1, &bitstr);
            seek += ret;
