b37147a2b482fc8b31206b17a8e16d3aab6324d2
Clb184 <84605945+Clb184@users.noreply.github.com>
2026-05-14 20:38:38 -0500

    Describe OpenGL error codes

--- include/Output.h
+++ include/Output.h
@@ -9,13 +9,27 @@
 #define LOG_INFO(x) fprintf(stdout, "[INFO] %s : %d - %s() | %s\n", __FILE__, __LINE__, __func__, x)
 #define LOG_ERROR(x) fprintf(stderr, "[ERROR] %s : %d - %s() | %s\n", __FILE__, __LINE__, __func__, x)
 
+static const char* GetGLErrorValueString(int e) {
+
+	switch(e) {
+		case 0x0500: return "INVALID ENUM";
+		case 0x0501: return "INVALID_VALUE";
+		case 0x0502: return "INVALID OPERATION";
+		case 0x0503: return "STACK OVERFLOW";
+		case 0x0504: return "STACK UNDERFLOW";
+		case 0x0505: return "OUT OF MEMORY";
+		case 0x0506: return "INVALID FRAMEBUFFER OPERATION";
+		case 0x0507: return "CONTEXT LOST";
+		default: return "Unknown Error";
+	}
+}
 
 #define GL_ERROR() while (e = glGetError()) {\
-	fprintf(stderr, "[GL ERROR] %s : %d > %s() failed with code %d\n", __FILE__, __LINE__, __func__, e);\
+	fprintf(stderr, "[GL ERROR] (%s) @ %s : %d > %s()\n", GetGLErrorValueString(e), __FILE__, __LINE__, __func__);\
 }
 
 #define GL_ERROR_RETURN() while (e = glGetError()) {\
-	fprintf(stderr, "[GL ERROR] %s : %d > %s() failed with code %d\n", __FILE__, __LINE__, __func__, e);\
+	fprintf(stderr, "[GL ERROR] (%s) @ %s : %d > %s()\n", GetGLErrorValueString(e), __FILE__, __LINE__, __func__);\
 	return false;\
 }
 
